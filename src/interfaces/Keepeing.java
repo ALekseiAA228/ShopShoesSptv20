fd7213c69e486249292f8a6ba070982cd1168721
IvanSerov <VanSer1989@gmail.com>
2021-11-08 10:51:54 +0200

    1

---
+++ src/interfaces/Keepeing.java
@@ -0,0 +1,19 @@
+
+package interfaces;
+
+
+import entity.Customer;
+import entity.Purchase;
+import entity.Shoes;
+import java.util.List;
+
+
+
+public interface Keepeing {
+    public void saveShoeses(List<Shoes> shoeses);
+    public List<Shoes> loadShoeses();
+    public void saveCustomers(List<Customer> customers);
+    public List<Customer> loadCustomers();
+    public void savePurchases(List<Purchase> purchases);
+    public List<Purchase> loadPurchases();
+}
